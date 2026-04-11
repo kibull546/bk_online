2616016316bb475278c7225aaa8fc55f3a781d89
kibull546 <kibull546@gmail.com>
2026-04-12 03:10:46 +0700

    fix: downgrade all symfony to v7 (railway compatible)

---
+++ app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ForceHttps
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->secure() && app()->environment('production')) {
+            return redirect()->secure($request->getRequestUri());
+        }
+        return $next($request);
+    }
+}
